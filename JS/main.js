59d81252a77f221fd6e9c0515c4406620d7b4f90
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2022-12-06 16:05:10 +0100

    inizio

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+// Attraverso l’apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email
+// e stamparli in pagina all’interno di una lista.
+// (chiaramente usando Vue, etc.,)
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const {createApp} = Vue
+
+// createApp({
+//     data(){
+//         return{
+
+//         }
+//     },
+//     methods:{
+        
+//     }
+// }).mount('#app')
+
+
+
+// posts.forEach((element, index, array) => {});
+
+// ciclo for console esplicativo 
+// for(let i = 0; i < arr.length; i++){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+// };
+
+// ciclo while console esplicativo 
+// let i = 0;
+// while(i < arr.length){
+//     console.log("arr[" + i + "]: " + arr[i]);
+
+//     i++;
+// };
+
+// for(){};
+
+// A = document.getElementById("B").value;
+
+
+//``
+
+// console.log("");
+// for(let i = 0; i < 10; i++){
+
+// }
+
+// }
